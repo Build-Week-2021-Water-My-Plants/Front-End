380345b9ccdb42b3846b6e871fdd74549c290d69
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-03-29 12:38:54 -0400

    Components created and routing has been set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //TECH IMPORTS
 import React, { useState, useEffect } from "react";
-import { Link, Route } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 import axios from "axios";
 import * as Yup from "yup";
 //STYLING IMPORTS
@@ -9,9 +9,23 @@ import "./styling/index.css";
 function App() {
   return (
    <div className="frontPageCatchAllDiv">
-     <Link to="/">Home</Link>
      <h1>Water My Plants</h1>
+     <nav>
+        <Link to="/">Home</Link>
+        <Link to="/CreateAccount">Create an Account</Link>
+        <Link to="/Login">Login</Link>
+        <Link to="/Profile">Profile</Link>
+      </nav>
      <h3>Keeping Your Plants Alive & Your Life Oxygenated</h3>
+
+     <Switch>
+        <Route path={'/CreateAccount'}></Route>
+        <Route path={'/Profile'}></Route>
+        <Route path={'/Login'}></Route>
+        <Route path={'/'}></Route>
+      </Switch>
+
+
    </div>
   );
 }
