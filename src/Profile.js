f9ea4b35682482a39fb83066c4c302f44d1ff4ae
Sergio Alvarez <salvare736@gmail.com>
2021-03-30 13:00:10 -0400

    added a default export function to 'Profile.js' so that the SPA wouldn't throw an error

--- src/Profile.js
+++ src/Profile.js
@@ -2,4 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../src/styling/index.css';
 
-
+export default function App() {
+  
+    return (
+      <div className='profile container'>
+        <h3>Here are your profile details:</h3>
+      </div>
+    );
+  };
